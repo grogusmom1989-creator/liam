d20af7894a6a301807e3355b4250d8caed7a7f59
grogusmom1989-creator <grogusmom1989-creator@users.noreply.github.com>
2026-09-24 07:52:37 -0400

    Add Sparkle Police PWA (game, pink badge icons, Pier 60 art)

---
+++ sparkle-police/sw.js
@@ -0,0 +1,8 @@
+const CACHE='sparkle-police-v1';
+const ASSETS=['/sparkle-police/','/sparkle-police/index.html','/sparkle-police/manifest.json',
+'/sparkle-police/icons/icon-192.png','/sparkle-police/icons/icon-512.png',
+'/sparkle-police/icons/icon-maskable-512.png','/sparkle-police/icons/apple-touch-icon.png',
+'/sparkle-police/pier60-launch.jpg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match('/sparkle-police/index.html'))));});
